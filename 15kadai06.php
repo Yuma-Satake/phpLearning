5b8760b0b1574ca2bf9da1088ef850148ac4649d
Yuma Satake <satous.y@icloud.com>
2023-04-27 16:42:03 +0900

    6

---
+++ 15kadai06.php
@@ -0,0 +1,7 @@
+<!-- 入力された二つの値を加算した結果を返却する関数calcSumの宣言 -->
+<?php
+function calcSum($num1, $num2) {
+    $sum = $num1 + $num2;
+    return $sum;
+}
+?>
---
+++ 15kadai06Use.php
@@ -0,0 +1,8 @@
+<?php
+// <!-- 15kadai06.phpをincludeする -->
+require_once("15kadai06.php");
+// calcSum関数を呼び出し、結果を変数に代入する
+$sum = calcSum(100, 200);
+// 結果を表示する
+echo "100 + 200 = " . $sum;
+?>
